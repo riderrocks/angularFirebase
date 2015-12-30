c7e731a93cdedc59dc48550f67137f2bbe7bcf04
Ananth Raghav <ananthraghavp@gmail.com>
2015-12-31 00:41:30 +0530

    optimized performance

---
+++ app/header/controller/header.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.header', ['ngRoute'])
+
+.controller('NavbarCtrl', ['$scope', '$location', 'CommonProp', function($scope, $location, CommonProp) {
+
+	$scope.isRouteActive = function(route) {
+		var curRoute = $location.path();
+		return curRoute.match(route);
+	}
+
+    $scope.logout = function() {
+        CommonProp.logoutUser();
+    }
+}]);
